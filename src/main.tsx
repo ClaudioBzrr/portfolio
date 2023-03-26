71cd188d8907bfdfd4437ce8cfb8af7c6491c465
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-03-25 21:03:37 -0300

    removed : blank lines

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,4 @@ import {App} from './App'
 import './assets/styles/global.css'
 
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
-)
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App/>)
