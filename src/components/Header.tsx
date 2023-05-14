943b084260d840369cceb19013291e2c6faf8909
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-05-13 22:32:13 -0300

    ✔ Added: header

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import { Box, Flex, Stack } from "@chakra-ui/react";
+
+export function Header(){
+    return(
+        <Flex w={'100vw'} h={'7vh'} position={'fixed'} top={0}>
+            <Box w={'100%'} h={'100%'} boxShadow={'lg'}>
+                <Stack w={'100%'} align={'center'}>
+                    
+                </Stack>
+            </Box>
+        </Flex>
+    )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
+import { Flex } from "@chakra-ui/react";
+import { Header } from "../components/Header";
 
 export function Home(){
     return(
-        <>
-        </>
+        <Flex h={'100vh'} w={'100vw'} bg={'gray.200'}>
+            <Header/>
+        </Flex>
     )
 }
