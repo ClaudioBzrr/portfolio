b75dca79cbf59826fce67e2c57ca6187fca3a8b9
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-05-15 19:50:56 -0300

    🗑 Removed: language translations

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { Box, Flex, Link, Stack } from "@chakra-ui/react";
 import { Link as ReactLink } from "react-router-dom";
-import { header } from "../language/header";
+
 
 export function Header(){
     return(
@@ -8,9 +8,9 @@ export function Header(){
             <Box w={'100%'} h={'100%'} boxShadow={'lg'}>
                 <Stack direction={"row"} justify={'center'} w={'100%'} h={'100%'} align={'center'}>
                     <Stack w={'40%'} h={'100%'} direction={"row"} align={'center'} justify={'center'}>
-                        <Link as={ReactLink} to={'/about'}>{header.portuguese.aboutLink}</Link>
-                        <Link as={ReactLink} to={'/skills'}>{header.portuguese.skillsLink}</Link>
-                        <Link as={ReactLink} to={'/contact'}>{header.portuguese.contactLink}</Link>
+                        <Link as={ReactLink} to={'/about'}>Sobre</Link>
+                        <Link as={ReactLink} to={'/skills'}>Habilidades</Link>
+                        <Link as={ReactLink} to={'/contact'}>Contato</Link>
                     </Stack>
                 </Stack>
             </Box>
