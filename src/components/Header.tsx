636b33789a1c5d75d234038546603002e251714a
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-05-15 20:38:39 -0300

    ✔ Added: color mode toggler button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
-import { Box, Flex, Link, Stack } from "@chakra-ui/react";
+import { Box, Flex, IconButton, Link, Stack, useColorMode} from "@chakra-ui/react";
 import { Link as ReactLink } from "react-router-dom";
+import {BsMoonStarsFill,BsSunFill} from 'react-icons/bs'
+
 
 
 export function Header(){
+    const {colorMode,toggleColorMode} = useColorMode()
+
     return(
         <Flex w={'100vw'} h={'7vh'} position={'fixed'} top={0}>
             <Box w={'100%'} h={'100%'} boxShadow={'lg'}>
@@ -11,6 +15,13 @@ export function Header(){
                         <Link as={ReactLink} to={'/about'}>Sobre</Link>
                         <Link as={ReactLink} to={'/skills'}>Habilidades</Link>
                         <Link as={ReactLink} to={'/contact'}>Contato</Link>
+                    </Stack>
+                    <Stack align={'center'} justify={'center'}>
+                        <IconButton
+                            onClick={toggleColorMode} 
+                            aria-label="color-mode toggler"
+                            icon={ colorMode == 'dark' ? <BsSunFill/> : <BsMoonStarsFill/>}
+                        />
                     </Stack>
                 </Stack>
             </Box>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,17 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, Stack, useColorModeValue } from "@chakra-ui/react";
 import { Header } from "../components/Header";
 
 export function Home(){
     return(
-        <Flex h={'100vh'} w={'100vw'} bg={'gray.200'}>
-            <Header/>
+        <Flex h={'100vh'} w={'100vw'} bg={useColorModeValue('gray.200','gray.700')}>
+            <Stack h={'100%'} w={'100%'}>
+                <Header/>
+                <Stack align={'center'} justify={'center'}  w={'100%'} h={'100%'}>
+                    <Box rounded={'xl'}  backdropFilter={'blur(50px)'} maxW={'90%'} w={'5xl'} h={'xl'} boxShadow={'lg'}>
+
+                    </Box>
+                </Stack>
+            </Stack>
         </Flex>
     )
 }
