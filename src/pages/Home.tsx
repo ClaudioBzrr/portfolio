25aeb8dc8aafd38f90d3fa2374e318f3d66c7817
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-05-20 09:42:29 -0300

    ✔ Added: dark theme

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,11 @@ import { Header } from "../components/Header";
 
 export function Home(){
     return(
-        <Flex h={'100vh'} w={'100vw'} bg={useColorModeValue('gray.200','gray.700')}>
+        <Flex h={'100vh'} w={'100vw'} bg={useColorModeValue('gray.200','blackAlpha.800')}>
             <Stack h={'100%'} w={'100%'}>
                 <Header/>
                 <Stack align={'center'} justify={'center'}  w={'100%'} h={'100%'}>
-                    <Box rounded={'xl'}  backdropFilter={'blur(50px)'} maxW={'90%'} w={'5xl'} h={'xl'} boxShadow={'lg'}>
+                    <Box rounded={'xl'}  backdropFilter={'blur(50px)'} maxW={'90%'} w={'5xl'} h={'xl'} boxShadow={'lg'} bg={useColorModeValue('gray.100','blackAlpha.400')}>
 
                     </Box>
                 </Stack>
