b283bab12f0732efff8407cff63f1ec3705b66d8
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-05-21 21:25:34 -0300

    ✔ Adedd: flex direction in box

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ export function Home(){
         <Flex h={'100vh'} w={'100vw'} bg={useColorModeValue('gray.200','blackAlpha.800')}>
             <Stack h={'100%'} w={'100%'}>
                 <Header/>
-                <Stack align={'center'} justify={'center'}  w={'100%'} h={'100%'}>
+                <Stack direction={'column'} align={'center'} justify={'center'}  w={'100%'} h={'100%'}>
                     <Box rounded={'xl'}  backdropFilter={'blur(50px)'} maxW={'90%'} w={'5xl'} h={'xl'} boxShadow={'lg'} bg={useColorModeValue('gray.100','blackAlpha.400')}>
 
                     </Box>
