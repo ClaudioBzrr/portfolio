572ef0604cd719cddb4431e2e33ad740fc94b2a3
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-07-24 21:14:30 -0300

    🩹 Fix: items spacing

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export function App() {
   return (
     <div className="bg-gray-950 max-w-screen min-h-screen flex flex-row items-center justify-center">
       <div className="flex flex-col items-center justify-center mx-10 pt-5">
-        <article className="max-h-screen">
+        <article className="h-screen flex flex-col justify-evenly">
           <section className="presentation">
             <div className="text-slate-300 sm:text-5xl text-2xl">
               <h1 className="w-full text-left">Olá 👋,</h1>
@@ -22,7 +22,7 @@ export function App() {
               Desenvolvedor Fullstack
             </p>
           </section>
-          <section className="Experience sm:mt-20 mt-10">
+          <section className="Experience mt-10">
             <div className="text-slate-300 md:text-2xl sm:text-xl">
               <p className="w-full">
                 Estudo React, React Native e Node js desde{' '}
