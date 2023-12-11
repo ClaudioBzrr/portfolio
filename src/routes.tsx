d65887dd8089bfc0a5922e0d9998c6afe92345b6
claudiobzrr <39713988+ClaudioBzrr@users.noreply.github.com>
2023-12-11 11:03:56 +0000

    ✔️ Added: skills page in routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { About } from './pages/About';
 import { Contact } from './pages/Contact';
 import { Home } from './pages/Home';
+import { Skills } from './pages/Skills';
 import { IRoute } from './types/route';
 
 export const routes: IRoute[] = [
@@ -13,6 +14,11 @@ export const routes: IRoute[] = [
     name: 'Sobre mim',
     path: '/about',
     element: <About />,
+  },
+  {
+    name: 'Skills',
+    path: '/skills',
+    element: <Skills />,
   },
   {
     name: 'Contato',
