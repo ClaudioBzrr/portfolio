353d569b2f1f1140814f2e0345f7b942d2acdb9e
claudiobzrr <39713988+ClaudioBzrr@users.noreply.github.com>
2024-02-21 13:48:27 +0000

    🩹 Fixed: bug with path filenames

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   basePath: '/portfolio',
+  assetPrefix: '/portfolio',
+  distDir: 'dist',
 };
 
 export default nextConfig;
